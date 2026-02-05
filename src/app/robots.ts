84507c2068fe0a9e9a83b1f1424e3d2e3ed95b00
lawdesk-ch <lawdesk.ch@gmail.com>
2026-02-05 19:15:35 +0200

    upd

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,5 +1,14 @@
-// import { MetadataRoute } from 'next';
+import { MetadataRoute } from 'next';
+import { SITE_URL } from '@/const/constants';
 
-// export default function robots(): MetadataRoute.Robots {
-//   return {};
-// }
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: ['/'],
+      disallow: ['/_next/', '/api/', '/*.json$', '/*?'],
+    },
+    sitemap: `${SITE_URL}/sitemap.xml`,
+    host: SITE_URL,
+  };
+}
